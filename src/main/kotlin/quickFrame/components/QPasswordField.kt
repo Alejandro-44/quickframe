bf1aa2d901cac5ba8958840d376ab5d56f392dc3
Alejandro <alejandroarguelloduarte@gmail.com>
2023-04-11 18:55:23 -0500

    adding javadocs for QPasswordField

--- src/main/kotlin/quickFrame/components/QPasswordField.kt
+++ src/main/kotlin/quickFrame/components/QPasswordField.kt
@@ -2,10 +2,21 @@ package quickFrame.components
 
 import javax.swing.JPasswordField
 
+/**
+ * This class represents a customized password field, which inherits from the JPasswordField class.
+ * It allows the setting of various properties and actions using a builder.
+ */
 class QPasswordField: JPasswordField() {
 
+    /**
+     * An instance of [QComponentBuilder] to apply properties to this [QPasswordField].
+     */
     private val builder = QComponentBuilder()
 
+    /**
+     * Applies a [block] of [QComponentBuilder] properties to this [QPasswordField].
+     * @param block a lambda expression with a receiver of type [QComponentBuilder].
+     */
     fun properties(block: QComponentBuilder.(QPasswordField) -> Unit) {
         builder.block(this)
     }
