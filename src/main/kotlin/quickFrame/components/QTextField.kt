7afdd2303e8a774f4e29df7a9d158883925fbb8c
Alejandro <alejandroarguelloduarte@gmail.com>
2023-04-11 08:45:33 -0500

    adding docs to QTextField

--- src/main/kotlin/quickFrame/components/QTextField.kt
+++ src/main/kotlin/quickFrame/components/QTextField.kt
@@ -1,14 +1,25 @@
 package quickFrame.components
 
 import javax.swing.JTextField
-
+/**
+ * custom implementation of the JTextField that provides a convenient way to set its properties.
+ */
 class QTextField: JTextField() {
 
+    /**
+     * An instance of [QComponentBuilder] to apply properties to this [QTextField].
+     */
     private val builder = QComponentBuilder()
 
+    /**
+     * Applies a [block] of [QComponentBuilder] properties to this [QTextField].
+     * @param block a lambda expression with a receiver of type [QComponentBuilder].
+     */
     fun properties(block: QComponentBuilder.(QTextField) -> Unit) {
         builder.block(this)
     }
-
-
 }
+
+
+
+
