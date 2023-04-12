4049802ded909e67e790504b0062ad55feb41d33
Alejandro <alejandroarguelloduarte@gmail.com>
2023-04-11 19:49:52 -0500

    Adding some java docs for QLabel

--- src/main/kotlin/quickFrame/components/QLabel.kt
+++ src/main/kotlin/quickFrame/components/QLabel.kt
@@ -2,10 +2,21 @@ package quickFrame.components
 
 import javax.swing.JLabel
 
+/**
+ * This class represents a customized label, which inherits from the JLabel class.
+ * It allows the setting of various properties and actions using a builder.
+ * @param text the text to display on the label
+ */
 class QLabel(text: String): JLabel(text) {
-
+    /**
+     * An instance of [QComponentBuilder] to apply properties to this [QLabel].
+     */
     private val builder = QComponentBuilder()
 
+    /**
+     * Applies a [block] of [QComponentBuilder] properties to this [QLabel].
+     * @param block a lambda expression with a receiver of type [QComponentBuilder].
+     */
     fun properties(block: QComponentBuilder.(QLabel) -> Unit) {
         builder.block(this)
     }
