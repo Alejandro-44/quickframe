ee4f62b8afccbd5ab233a3f04f95243b09507c41
Alejandro <alejandroarguelloduarte@gmail.com>
2023-04-11 19:10:17 -0500

    adding javadocs for QComboBox

--- src/main/kotlin/quickFrame/components/QComboBox.kt
+++ src/main/kotlin/quickFrame/components/QComboBox.kt
@@ -3,19 +3,41 @@ package quickFrame.components
 
 import javax.swing.JComboBox
 
+/**
+ * This class represents a customized combo box, which inherits from the JComboBox class.
+ * It allows the setting of various properties and actions using a builder.
+ */
 class QComboBox<T>: JComboBox<T>() {
 
+    /**
+     * An instance of [QComponentBuilder] to apply properties to this [QComboBox].
+     */
     private val builder = QComponentBuilder()
 
+    /**
+     * Applies a [block] of [QComponentBuilder] properties to this [QComboBox].
+     * @param block a lambda expression with a receiver of type [QComponentBuilder].
+     */
     fun properties(block: QComponentBuilder.(QComboBox<T>) -> Unit) {
         builder.block(this)
     }
 
+    /**
+     * Set the items of the QComboBox instance with an array of items.
+     *
+     * @param items the array of items to set.
+     */
     infix fun items(items: Array<T>) {
         items.forEach { addItem(it) }
     }
 
+    /**
+     * Set the selected item of the QComboBox instance to the item at the specified index.
+     *
+     * @param i the index of the item to select.
+     */
     infix fun selectedItem(i: Int) {
         this.selectedItem = i
     }
+
 }
