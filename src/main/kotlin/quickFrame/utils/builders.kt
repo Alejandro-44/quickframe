1cad7d81860709ac57cb5804a14519699078eae7
Alejandro <alejandroarguelloduarte@gmail.com>
2023-04-11 19:53:56 -0500

    Adding comments for builders

--- src/main/kotlin/quickFrame/utils/builders.kt
+++ src/main/kotlin/quickFrame/utils/builders.kt
@@ -3,12 +3,24 @@ package quickFrame.utils
 import quickFrame.components.QForm
 import quickFrame.containers.QFrame
 
+/**
+ * Creates and returns a QFrame instance with the specified build instructions.
+ * @param build A lambda expression that takes a [QFrame] instance as its receiver and returns [Unit].
+ *  This lambda is used to configure the properties of the [QFrame].
+ *  @return A new [QFrame] instance with the specified properties.
+ */
 fun buildFrame(build: QFrame.() -> Unit): QFrame {
     val frame = QFrame()
     frame.build()
     return frame
 }
 
+/**
+ * Creates and returns a QForm instance with the specified build instructions.
+ * @param build A lambda expression that takes a [QForm] instance as its receiver and returns [Unit].
+ *   This lambda is used to configure the properties of the [QForm].
+ * @return @return A new [QForm] instance with the specified properties.
+ */
 fun buildForm(build: QForm.() -> Unit): QForm {
     val form = QForm()
     form.build()
